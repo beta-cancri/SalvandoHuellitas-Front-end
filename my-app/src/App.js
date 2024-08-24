5ad0888fc52e32e9b594c1cf83147b023970604e
Mairacol <120331883+Mairacol@users.noreply.github.com>
2024-08-24 16:35:40 -0300

    detail-contact-about iniciados

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import About from './views/about/about.component.jsx'; 
+import Contact from './views/contact/contact.component.jsx';
+import Detail from './views/detail/detail.component.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/detail/:id" element={<Detail />} />
+
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
