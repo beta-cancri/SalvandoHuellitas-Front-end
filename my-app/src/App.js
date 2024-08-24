977c420a09f20b6daff95c6e55de31968741b13f
Diego Chinchay <149448905+beta-cancri@users.noreply.github.com>
2024-08-24 14:13:34 -0500

    home, landing, searchbar, redux

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar/navbar.component';
+import Home from './views/home/home.component';
+import Detail from './views/detail/detail.component';
+import About from './views/about/about.component';
+import Landing from './views/landing/landing.component';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/about" element={<About />} />
+        {/* Additional routes can be added here */}
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
