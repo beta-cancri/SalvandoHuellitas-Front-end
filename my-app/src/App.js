4928fecea47bc654a4b1fa2f9379aeb6928c4c19
Mairacol <120331883+Mairacol@users.noreply.github.com>
2024-08-24 21:45:54 -0300

    modifique detalles en detail-contact-about-navbar-landing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,33 +1,32 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom'; // Asegúrate de importar correctamente
 import Navbar from './components/navbar/navbar.component';
 import Home from './views/home/home.component';
 import Detail from './views/detail/detail.component';
 import About from './views/about/about.component';
 import Landing from './views/landing/landing.component';
-import Contact from './views/contact/contact.component.jsx';
+import Contact from './views/contact/contact.component';
 import './App.css';
 
 const App = () => {
   const location = useLocation();
   
-  // Check if the current route is the landing page
+  // Verifica si la ruta actual es la página de inicio
   const showNavbar = location.pathname !== '/';
 
   return (
-    <Router>
-      <div className="App">
-        {showNavbar && <Navbar />}
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/detail/:id" element={<Detail />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      {showNavbar && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
   );
 };
 
 export default App;
+
