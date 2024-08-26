30376f65190a3ac1816b03bd12a516eeb16cc99c
MeliAviles <mel.avilesg@gmail.com>
2024-08-26 15:50:01 -0600

    handle redirection into home and send token to backend

--- my-app/src/auth/auth.js
+++ my-app/src/auth/auth.js
@@ -1,5 +1,6 @@
-const clientId = '1092527296947-6aebqgtuhuujsgkpll0efhpolndl1vvk.apps.googleusercontent.com'; 
-const redirectUri = 'http://localhost:3001/auth/google'; // Change to your frontend URL and endpoint
+import axios from 'axios';
+const clientId = '1092527296947-6aebqgtuhuujsgkpll0efhpolndl1vvk.apps.googleusercontent.com';
+const redirectUri = 'http://localhost:3000/home'; // Change to your frontend URL and endpoint
 
 export function iniciarAutenticacion() {
   const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=token&scope=email profile`;
@@ -13,8 +14,8 @@ export function manejarRedireccion() {
 
   // Store the token in local storage (optional)
   if (accessToken) {
-    localStorage.setItem('accessToken', accessToken);
-    // Optionally, redirect to a protected page
-    window.location.href = '/home'; // Redirect to a protected route after login
+    axios.post("http://localhost:3001/auth/google/", {
+      token: accessToken,
+    })
   }
 }
