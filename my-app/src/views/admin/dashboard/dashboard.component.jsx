cf0dd0b5bbd0a48caf3b782555bcf916156e226f
Diego Chinchay <149448905+beta-cancri@users.noreply.github.com>
2024-09-07 13:12:49 -0500

    dashboard update

--- my-app/src/views/admin/dashboard/dashboard.component.jsx
+++ my-app/src/views/admin/dashboard/dashboard.component.jsx
@@ -1,18 +1,72 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import ManagePets from '../managepets/managepets.component';
 import ManageRequests from '../managerequests/managerequests.component';
 import ManageUser from '../manageuser/manageuser.component';
+import Select from 'react-select';
+import { fetchPets } from '../../../redux/actions';
 import './dashboard.styles.css';
 
 const AdminDashboard = () => {
-
-  
-
-
   const [activeSection, setActiveSection] = useState(null);
+  const dispatch = useDispatch();
+  const { currentPage, totalPages } = useSelector((state) => state);
 
+  // Filter state for status
+  const [status, setStatus] = useState('');
+
+  // Handle section change
   const handleSectionChange = (section) => {
     setActiveSection(section);
+  };
+
+  // Handle filter change
+  const handleFilterChange = (selectedOption) => {
+    setStatus(selectedOption ? selectedOption.value : '');
+  };
+
+  // Fetch pets whenever the status changes
+  useEffect(() => {
+    if (activeSection === 'pets') {
+      dispatch(fetchPets({ status }, 1, false));
+    }
+  }, [dispatch, status, activeSection]);
+
+  // Filter options for status
+  const statusOptions = [
+    { value: '', label: 'Todos' },
+    { value: 'available', label: 'Activo' },
+    { value: 'inactive', label: 'Inactivo' }
+  ];
+
+  // Custom styles for Select dropdown
+  const customStyles = {
+    control: (provided) => ({
+      ...provided,
+      width: '100%',
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      borderColor: '#fff',
+      boxShadow: 'none',
+      borderRadius: '4px'
+    }),
+    menu: (provided) => ({
+      ...provided,
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      borderRadius: '4px'
+    }),
+    option: (provided, state) => ({
+      ...provided,
+      backgroundColor: state.isSelected ? '#a8a3a3' : state.isFocused ? '#f0cd8a' : 'rgba(0, 0, 0, 0.5)',
+      color: state.isSelected ? '#000' : '#fff',
+    }),
+    singleValue: (provided) => ({
+      ...provided,
+      color: '#fff'
+    }),
+    placeholder: (provided) => ({
+      ...provided,
+      color: '#a8a3a3'
+    })
   };
 
   return (
@@ -28,9 +82,27 @@ const AdminDashboard = () => {
         <button className="sidebar-button" onClick={() => handleSectionChange('requests')}>
           Ver peticiones de adopción
         </button>
+
+        {activeSection === 'pets' && (
+          <div className="filter-controls-admin">
+            <label>
+              Estado:
+              <Select
+                className="custom-select-container-admin"
+                classNamePrefix="custom-select-admin"
+                value={statusOptions.find(option => option.value === status)}
+                onChange={handleFilterChange}
+                options={statusOptions}
+                styles={customStyles}
+                isClearable
+              />
+            </label>
+          </div>
+        )}
       </div>
+
       <div className="dashboard-display">
-        {activeSection === 'pets' && <ManagePets />}
+        {activeSection === 'pets' && <ManagePets status={status} />}
         {activeSection === 'users' && <ManageUser />}
         {activeSection === 'requests' && <ManageRequests />}
       </div>
@@ -39,6 +111,7 @@ const AdminDashboard = () => {
 };
 
 export default AdminDashboard;
+
 
 
 
--- my-app/src/views/admin/managepets/managepets.component.jsx
+++ my-app/src/views/admin/managepets/managepets.component.jsx
@@ -1,19 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchPets, changePetStatus } from '../../../redux/actions';
-import Select from 'react-select';
 import './managepets.styles.css';
 
-const ManagePets = () => {
+const ManagePets = ({ status }) => {
   const dispatch = useDispatch();
   const { pets, currentPage, totalPages } = useSelector((state) => state);
 
-  // Filter state for status
-  const [status, setStatus] = useState('');
-
   // Fetch pets with status filter
   useEffect(() => {
-    dispatch(fetchPets({ status }, 1, false)); // Fetch all pets for admin dashboard with filtering
+    dispatch(fetchPets({ status }, 1, false));
   }, [dispatch, status]);
 
   // Handle status change
@@ -25,7 +21,7 @@ const ManagePets = () => {
       dispatch(changePetStatus(petId, newStatus))
         .then(() => {
           // Refetch pets after status change
-          dispatch(fetchPets({ status }, currentPage, false)); // Ensure pets are refetched after status change
+          dispatch(fetchPets({ status }, currentPage, false));
         })
         .catch((error) => {
           console.error('Error changing status:', error);
@@ -33,75 +29,17 @@ const ManagePets = () => {
     }
   };
 
-  // Handle status filter change
-  const handleFilterChange = (selectedOption) => {
-    setStatus(selectedOption ? selectedOption.value : '');
-  };
-
   // Pagination handling
   const handlePageChange = (pageNumber) => {
     dispatch(fetchPets({ status }, pageNumber, false));
-  };
-
-  // Filter options for status
-  const statusOptions = [
-    { value: '', label: 'Todos' },
-    { value: 'available', label: 'Activo' },
-    { value: 'inactive', label: 'Inactivo' }
-  ];
-
-  // Custom styles for Select dropdown
-  const customStyles = {
-    control: (provided) => ({
-      ...provided,
-      width: '100%',
-      backgroundColor: 'rgba(0, 0, 0, 0.5)',
-      borderColor: '#fff',
-      boxShadow: 'none',
-      borderRadius: '4px'
-    }),
-    menu: (provided) => ({
-      ...provided,
-      backgroundColor: 'rgba(0, 0, 0, 0.5)',
-      borderRadius: '4px'
-    }),
-    option: (provided, state) => ({
-      ...provided,
-      backgroundColor: state.isSelected ? '#a8a3a3' : state.isFocused ? '#f0cd8a' : 'rgba(0, 0, 0, 0.5)',
-      color: state.isSelected ? '#000' : '#fff',
-    }),
-    singleValue: (provided) => ({
-      ...provided,
-      color: '#fff'
-    }),
-    placeholder: (provided) => ({
-      ...provided,
-      color: '#a8a3a3'
-    })
   };
 
   return (
     <div className="manage-pets">
       <div className="manage-pets-header">
         <h2>Administrar Mascotas</h2>
-  
-        {/* Filter by status */}
-        <div className="filter-controls-admin">
-          <label>
-            Estado:
-            <Select
-              className="custom-select-container-admin"
-              classNamePrefix="custom-select-admin"
-              value={statusOptions.find(option => option.value === status)}
-              onChange={handleFilterChange}
-              options={statusOptions}
-              styles={customStyles}
-              isClearable
-            />
-          </label>
-        </div>
       </div>
-  
+
       {pets && pets.length > 0 ? (
         <div className="pets-container-admin">
           <ul className="pets-grid-admin">
@@ -130,7 +68,7 @@ const ManagePets = () => {
               </li>
             ))}
           </ul>
-          
+
           {/* Pagination */}
           <div className="pagination-admin">
             <button onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1}>
