56c315795de2b935590293b70e348fe4d85fdf95
Mlsanchezvilla <sanchezvilla.2298@gmail.com>
2024-09-14 10:39:35 -0500

    se quito codigo comentado en algunos componentes para limpiarlos

--- my-app/src/views/reviews/validationForReviews.js
+++ my-app/src/views/reviews/validationForReviews.js
@@ -1,30 +1,3 @@
-// const validationReviews = (formData) => {
-//   let errors = {};
-
-//   if (!formData.fullName.trim()) {
-//     errors.fullName = "Por favor, ingrese su nombre completo";
-//     return errors;
-//   } else if (!/^[a-zA-Z\s]+$/.test(formData.fullName)) {
-//     errors.fullName =
-//       "Su nombre no debe contener numeros o caracteres especiales";
-//     return errors;
-//   }
-
-//   if (!formData.date) {
-//     errors.date = "Por favor, ingrese la fecha de la reseña";
-//     return errors;
-//   }
-
-//   if (!formData.occupation) {
-//     errors.occupation = "Por favor, dinos a qué te de dedicas";
-//     return errors;
-//   }
-
-//   return errors;
-// };
-
-// export default validationReviews;
-
 const validate = (formData) => {
   let errors = {};
 
