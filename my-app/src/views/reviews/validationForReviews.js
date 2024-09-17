7af2938e8c41c06f163f7fa5bae74e8fefa842ff
Mlsanchezvilla <sanchezvilla.2298@gmail.com>
2024-09-17 17:30:56 -0500

    implementacion de las reseñas

--- my-app/src/views/reviews/validationForReviews.js
+++ my-app/src/views/reviews/validationForReviews.js
@@ -1,30 +1,3 @@
-// const validationReviews = (formData) => {
-//   let errors = {};
-
-//   if (!formData.fullName.trim()) {
-//     errors.fullName = "Por favor, ingrese su nombre completo";
-//     return errors;
-//   } else if (!/^[a-zA-Z\s]+$/.test(formData.fullName)) {
-//     errors.fullName =
-//       "Su nombre no debe contener numeros o caracteres especiales";
-//     return errors;
-//   }
-
-//   if (!formData.date) {
-//     errors.date = "Por favor, ingrese la fecha de la reseña";
-//     return errors;
-//   }
-
-//   if (!formData.occupation) {
-//     errors.occupation = "Por favor, dinos a qué te de dedicas";
-//     return errors;
-//   }
-
-//   return errors;
-// };
-
-// export default validationReviews;
-
 const validate = (formData) => {
   let errors = {};
 
@@ -43,11 +16,6 @@ const validate = (formData) => {
   // Validación para la calificación
   if (!formData.rating || formData.rating === "") {
     errors.rating = "Por favor, selecciona una calificación.";
-  }
-
-  // Validación para la fecha
-  if (!formData.date || formData.date === "") {
-    errors.date = "Por favor, ingresa la fecha.";
   }
 
   return errors;
